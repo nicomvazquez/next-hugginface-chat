8b1c8510a4981ea80883f9a9bc7523d7cf00a538
nicomvazquez <vazquezpizzinicolas@gmail.com>
2024-06-30 17:14:39 -0300

    first commit

---
+++ src/app/api/chat/route.js
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import { HfInference } from "@huggingface/inference";
+import { HuggingFaceStream, StreamingTextResponse } from "ai";
+import { experimental_buildOpenAssistantPrompt } from "ai/prompts";
+
+const hf = new HfInference("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");
+
+export async function POST(request) {
+  const { messages } = await request.json();
+
+  const response = hf.textGenerationStream({
+    model: "OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5",
+    inputs: experimental_buildOpenAssistantPrompt(messages),
+    parameters: {
+      max_new_tokens: 200,
+      // @ts-ignore (this is a valid parameter specifically in OpenAssistant models)
+      typical_p: 0.2,
+      repetition_penalty: 1,
+      truncate: 1000,
+      return_full_text: false,
+    },
+  });
+
+  // Convert the response into a friendly text-stream
+  const stream = HuggingFaceStream(response);
+
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,65 @@
+"use client";
+import React from "react";
+
+import { useChat } from "ai/react";
+
+function page() {
+  const { handleInputChange, handleSubmit, input, isLoading, messages } =
+    useChat();
+
+  console.log(messages);
+
+  return (
+    <section className="flex justify-center items-end h-svh p-5">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col justify-between h-full max-w-xl w-full"
+      >
+        <div>
+          <h1 className="text-4xl font-semibold text-white">AI chat bot</h1>
+        </div>
+        <div className="text-white max-h-full h-full my-5 overflow-y-auto">
+          {messages.map((m) => (
+            <div
+              key={m.id}
+              className={`flex flex-col gap-3 text-lg mb-2 p-5 rounded-2xl ${
+                m.role === "assistant"
+                  ? "self-start text-left mr-10 "
+                  : "self-end text-right ml-10 bg-gray-600"
+              }`}
+            >
+              <span
+                className={`text-xs font-light ${
+                  m.role === "assistant" ? "text-left" : "text-right"
+                }`}
+              >
+                {m.role}
+              </span>{" "}
+              {m.content}
+            </div>
+          ))}
+        </div>
+        <div className="flex justify-between my-4">
+          <label className="text-white block font-bold my-2">
+            Respondere tus preguntas...
+          </label>
+          <button
+            className="bg-green-500 text-white px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-green-600 disabled:opacity-50"
+            disabled={isLoading || !input}
+          >
+            Send
+          </button>
+        </div>
+        <input
+          value={input}
+          onChange={handleInputChange}
+          className="text-black bg-slate-300 px-3 py-2 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
+          placeholder="Capital de francia..."
+          autoFocus
+        />
+      </form>
+    </section>
+  );
+}
+
+export default page;
